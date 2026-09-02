766cf0062f2d7c2d4f818b9ba75a8b82b5d706d4
senezz <senezzyx@gmail.com>
2026-09-02 17:04:07 +0200

    feat(jobs): wire up jobs router in server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { authRouter } from "./auth/auth.router";
+import { jobsRouter } from "./jobs/jobs.router";
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.get("/health", (_req, res) => {
 });
 
 app.use("/auth", authRouter);
+app.use("/jobs", jobsRouter);
 
 const port = process.env.PORT ?? 4000;
 app.listen(port, () => {
